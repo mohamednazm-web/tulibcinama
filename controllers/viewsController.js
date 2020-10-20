f65c9a4294f8caa511d7c2a1b9c578640d54064a
mohamednazm-web <mhamadnazm53@gmail.com>
2020-10-19 18:07:42 -0700

    make it better

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -27,14 +27,19 @@ exports.posters = catchAsync(async (req, res, next) => {
         } */
     });
 
-    
+    const profile = await Profile.find({
+        numOfPosters: {
+            $gte: 5
+        }
+    })
 
     // 2) Build template
     // 3) Render that template using tour data from 1)
     res.status(200).render('home', {
         title: 'home page',
         posterOne,
-        posterTwo
+        posterTwo,
+        profile
     });
 });
 
